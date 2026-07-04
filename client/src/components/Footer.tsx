827b7f0a9f8c3e7b40e50c7131162115c52e9e39
Vivek Sahu <viveksahu1762@gmail.com>
2026-07-04 19:41:59 +0530

    Update .gitignore files, remove favicon, and update Footer link

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="z-30 border-t-4 border-black bg-white px-5 py-5 text-center font-base m500:text-sm">
       Made with love by team{" "}
       <a
-        href="https://github.com/TechieeGeeeks/incasino"
+        href="https://github.com/vwakesahu/incasino"
         className="underline font-heading">
         Incasino
       </a>
